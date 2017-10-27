bbaa36f905122a69b6b0a9d2350789cc4948c293
jimmyyou <jieyou@umich.edu>
2017-10-27 16:55:52 -0400

    Add submitShuffleInfo()

--- src/main/java/edu/umich/gaialib/GaiaClient.java
+++ src/main/java/edu/umich/gaialib/GaiaClient.java
@@ -5,50 +5,84 @@ package edu.umich.gaialib;
 
 import io.grpc.*;
 
-import java.io.IOException;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import edu.umich.gaialib.gaiaprotos.*;
 
-
 public class GaiaClient {
-  private static final Logger logger = Logger.getLogger(GaiaClient.class.getName());
+    private static final Logger logger = Logger.getLogger(GaiaClient.class.getName());
 
-  private final ManagedChannel channel;
-  private final GaiaShuffleGrpc.GaiaShuffleBlockingStub blockingStub;
+    private final ManagedChannel channel;
+    private final GaiaShuffleGrpc.GaiaShuffleBlockingStub blockingStub;
 
-  /** Construct client connecting to HelloWorld server at {@code host:port}. */
-  public GaiaClient(String host, int port) {
-    this(ManagedChannelBuilder.forAddress(host, port)
-        // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
-        // needing certificates.
-        .usePlaintext(true)
-        .build());
-  }
-
-  /** Construct client for accessing RouteGuide server using the existing channel. */
-  GaiaClient(ManagedChannel channel) {
-    this.channel = channel;
-    blockingStub = GaiaShuffleGrpc.newBlockingStub(channel);
-  }
-
-  public void shutdown() throws InterruptedException {
-    channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
-  }
-
-  /** Say hello to server. */
-  public void greet(String name) {
-    logger.info("Will try to greet " + name + " ...");
-    ShuffleInfo request = ShuffleInfo.newBuilder().setName(name).build();
-    ShuffleInfoReply response;
-    try {
-      response = blockingStub.submitShuffleInfo(request);
-    } catch (StatusRuntimeException e) {
-      logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
-      return;
+    /**
+     * Construct client connecting to Gaia Controller server at {@code host:port}.
+     */
+    public GaiaClient(String host, int port) {
+        this(ManagedChannelBuilder.forAddress(host, port)
+                // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
+                // needing certificates.
+                .usePlaintext(true)
+                .build());
     }
-    logger.info("Greeting: " + response.getMessage());
-  }
+
+    /**
+     * Construct client for accessing Gaia Controller server using the existing channel.
+     */
+    GaiaClient(ManagedChannel channel) {
+        this.channel = channel;
+        blockingStub = GaiaShuffleGrpc.newBlockingStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    /**
+     * Say hello to server.
+     */
+    public void greet(String name) {
+        logger.info("Will try to greet " + name + " ...");
+        ShuffleInfo request = ShuffleInfo.newBuilder().setUsername(name).build();
+        ShuffleInfoReply response;
+        try {
+            response = blockingStub.submitShuffleInfo(request);
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return;
+        }
+        logger.info("Greeting: " + response.getMessage());
+    }
+
+    /**
+     * Submit ShuffleInfo to Gaia Controller
+     */
+    public void submitShuffleInfo(String username, String jobID, Map<String, String> mappersIP, Map<String, String> reducersIP) {
+        logger.info("Try to submit ShuffleInfo to controller");
+
+        ShuffleInfo.Builder sinfoBuiler = ShuffleInfo.newBuilder();
+        sinfoBuiler.setJobID(jobID).setUsername(username);
+
+        for (Map.Entry<String, String> me : mappersIP.entrySet()) {
+            sinfoBuiler.addMappers( ShuffleInfo.MapperInfo.newBuilder().setMapperID(me.getKey()).setMapperIP(me.getValue()));
+        }
+
+        for (Map.Entry<String, String> re : reducersIP.entrySet()) {
+            sinfoBuiler.addReducers( ShuffleInfo.ReducerInfo.newBuilder().setReducerID(re.getKey()).setReducerIP(re.getValue()));
+        }
+
+        ShuffleInfoReply response;
+
+        try {
+            response = blockingStub.submitShuffleInfo(sinfoBuiler.build());
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return;
+        }
+
+        logger.info("Gaia controller returned: " + response.getMessage());
+    }
 }
--- src/main/java/edu/umich/gaialib/GaiaServer.java
+++ src/main/java/edu/umich/gaialib/GaiaServer.java
@@ -22,6 +22,7 @@ import edu.umich.gaialib.gaiaprotos.ShuffleInfoReply;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
+
 import java.io.IOException;
 import java.util.logging.Logger;
 
@@ -29,51 +30,55 @@ import java.util.logging.Logger;
  * Server that manages startup/shutdown of a {@code Greeter} server.
  */
 public class GaiaServer {
-  private static final Logger logger = Logger.getLogger(GaiaServer.class.getName());
+    private static final Logger logger = Logger.getLogger(GaiaServer.class.getName());
 
-  private Server server;
+    private Server server;
 
-  private void start() throws IOException {
+    private void start() throws IOException {
     /* The port on which the server should run */
-    int port = 50051;
-    server = ServerBuilder.forPort(port)
-        .addService(new GreeterImpl())
-        .build()
-        .start();
-    logger.info("Server started, listening on " + port);
-    Runtime.getRuntime().addShutdownHook(new Thread() {
-      @Override
-      public void run() {
-        // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-        System.err.println("*** shutting down gRPC server since JVM is shutting down");
-        GaiaServer.this.stop();
-        System.err.println("*** server shut down");
-      }
-    });
-  }
-
-  private void stop() {
-    if (server != null) {
-      server.shutdown();
+        int port = 50051;
+        server = ServerBuilder.forPort(port)
+                .addService(new ShuffleServiceImpl())
+                .build()
+                .start();
+        logger.info("Server started, listening on " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                GaiaServer.this.stop();
+                System.err.println("*** server shut down");
+            }
+        });
     }
-  }
 
-  /**
-   * Await termination on the main thread since the grpc library uses daemon threads.
-   */
-  private void blockUntilShutdown() throws InterruptedException {
-    if (server != null) {
-      server.awaitTermination();
+    private void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
     }
-  }
 
-  static class GreeterImpl extends GaiaShuffleGrpc.GaiaShuffleImplBase {
-
-    @Override
-    public void submitShuffleInfo(ShuffleInfo req, StreamObserver<ShuffleInfoReply> responseObserver) {
-      ShuffleInfoReply reply = ShuffleInfoReply.newBuilder().setMessage("Hello " + req.getName()).build();
-      responseObserver.onNext(reply);
-      responseObserver.onCompleted();
+    /**
+     * Await termination on the main thread since the grpc library uses daemon threads.
+     */
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
     }
-  }
+
+    static class ShuffleServiceImpl extends GaiaShuffleGrpc.GaiaShuffleImplBase {
+
+        @Override
+        public void submitShuffleInfo(ShuffleInfo req, StreamObserver<ShuffleInfoReply> responseObserver) {
+            /**
+             * do the job here
+             */
+
+            ShuffleInfoReply reply = ShuffleInfoReply.newBuilder().setMessage("Hello " + req.getUsername()).build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
+    }
 }
