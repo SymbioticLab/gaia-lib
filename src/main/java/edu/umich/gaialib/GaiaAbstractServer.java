756326cbf780db92eeaef5bdd1dee4b989e8875b
jimmyyou <jieyou@umich.edu>
2017-10-30 11:49:33 -0400

    Add GaiaAbstractServer

--- src/main/java/edu/umich/gaialib/GaiaAbstractServer.java
+++ src/main/java/edu/umich/gaialib/GaiaAbstractServer.java
@@ -4,19 +4,20 @@ import edu.umich.gaialib.gaiaprotos.*;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
-
 import java.io.IOException;
 import java.util.logging.Logger;
 
-/**
- * Server that manages startup/shutdown of a {@code Greeter} server.
- */
-public class GaiaServer {
+public abstract class GaiaAbstractServer {
     private static final Logger logger = Logger.getLogger(GaiaServer.class.getName());
 
     private Server server;
+    private int port = 50001;
 
-    private void start() throws IOException {
+    public GaiaAbstractServer(int port) {
+        this.port = port;
+    }
+
+    public void start() throws IOException {
     /* The port on which the server should run */
         int port = 50051;
         server = ServerBuilder.forPort(port)
@@ -29,13 +30,13 @@ public class GaiaServer {
             public void run() {
                 // Use stderr here since the logger may have been reset by its JVM shutdown hook.
                 System.err.println("*** shutting down gRPC server since JVM is shutting down");
-                GaiaServer.this.stop();
+                GaiaAbstractServer.this.stop();
                 System.err.println("*** server shut down");
             }
         });
     }
 
-    private void stop() {
+    public void stop() {
         if (server != null) {
             server.shutdown();
         }
@@ -44,13 +45,13 @@ public class GaiaServer {
     /**
      * Await termination on the main thread since the grpc library uses daemon threads.
      */
-    private void blockUntilShutdown() throws InterruptedException {
+    public void blockUntilShutdown() throws InterruptedException {
         if (server != null) {
             server.awaitTermination();
         }
     }
 
-    static class ShuffleServiceImpl extends GaiaShuffleGrpc.GaiaShuffleImplBase {
+    class ShuffleServiceImpl extends GaiaShuffleGrpc.GaiaShuffleImplBase {
         @Override
         public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
             HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
@@ -60,16 +61,9 @@ public class GaiaServer {
 
         @Override
         public void submitShuffleInfo(ShuffleInfo req, StreamObserver<ShuffleInfoReply> responseObserver) {
-            /**
-             * do the job here
-             */
 
-            System.out.println(req.getUsername());
-            System.out.println(req.getJobID());
-            System.out.println(req.getMappers(0).getMapperIP());
-            System.out.println(req.getMappers(0).getMapperID());
-            System.out.println(req.getReducers(0).getReducerIP());
-            System.out.println(req.getReducers(0).getReducerID());
+            // do the job abstractly
+            processReq(req);
 
             ShuffleInfoReply reply = ShuffleInfoReply.newBuilder().setMessage("Hello " + req.getUsername()).build();
             responseObserver.onNext(reply);
@@ -77,9 +71,6 @@ public class GaiaServer {
         }
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        GaiaServer gaiaServer = new GaiaServer();
-        gaiaServer.start();
-        gaiaServer.blockUntilShutdown();
-    }
+    public abstract void processReq(ShuffleInfo req);
+
 }
