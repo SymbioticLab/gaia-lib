747de85999f91b3484f8756a5841e395c680d5db
jimmyyou <jieyou@umich.edu>
2017-11-02 18:10:09 -0400

    add mkdir -p

--- src/main/java/edu/umich/gaialib/GaiaTestServerSCP.java
+++ src/main/java/edu/umich/gaialib/GaiaTestServerSCP.java
@@ -50,7 +50,7 @@ public class GaiaTestServerSCP {
 //                String dstIP = req.getReducersList().get(i).getReducerIP().split(":",2)[0];
 //                String srcIP = req.getMappersList().get(i).getMapperIP().split(":",2)[0];
 
-                String cmd_mkdir = "ssh wentingt@" + dstIP + " 'mkdir " + trimmedData + "'";
+                String cmd_mkdir = "ssh wentingt@" + dstIP + " 'mkdir -p" + trimmedData + "'";
 
                 String cmd = "scp -r " + srcIP + ":" + trimmedData + "/* " + dstIP + ":" + trimmedData;
 
